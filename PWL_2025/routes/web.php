ac8f1f1d93f5ff24008f53f684b6c393a324c310
FaliIrham <falimagemaulana64@gmail.com>
2025-03-07 16:17:06 +0700

    Praktikum 1

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Soal No 2 Praltikum 1
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+//Soal No 4 Praltikum 1
+Route::get('/world', function () {
+    return 'World';
+});
+
+//Soal No 6 Praltikum 1
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+//Soal No 7 Praltikum 1
+Route::get('/about', function () {
+    return '2341720121 <br>
+            Fali irham Maulana';
+});
+
+//Soal No 8 Praltikum 1
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+});
+
+//Soal No 11 Praltikum 1
+Route::get('/posts/{posts}/coments/{coments}', function ($postId, $comentId) {
+    return 'Pos ke-' .$postId. "Komentar ke-" .$comentId ;
+});
+
+//Soal no 13 Praktikum 1
+Route::get('/articles/{id}', function ($articleid){
+    return 'Halaman Artikel dengan ID ' .$articleid;
+});
+
+//Soal no 14 & 17Praktikum 1
+Route::get('/user/{name?}', function ($name = 'Jhon'){
+    return 'Nama saya ' .$name;
 });
