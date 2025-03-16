aa0693add8dd6efde0918214fb3d00e0dd7d4690
FaliIrham <falimagemaulana64@gmail.com>
2025-03-16 17:51:07 +0700

    Praktikum 2.2 JS4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,12 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index (){
-
-        // $user = UserModel::find(1);
-        //$user = UserModel::firstWhere('level_id', 1 );
-        $user = UserModel::findOr(20, ['username', 'nama'], function (){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
